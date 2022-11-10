d09f6829129912ca9960c388fa1deeeaf6ebd6c2
Do Hai <dohaizxc@gmail.com>
2022-11-10 14:29:19 +0700

    test

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
   return (
     <Layout>
       <Breadcrumb style={{ marginLeft: "16px" }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Xin chào</Breadcrumb.Item>
       </Breadcrumb>
       <div className="p-[24px] min-h-[360px] bg-white m-[24px]">
         Home
