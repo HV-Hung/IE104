364468381c50a9ae77df4d2b3893fce5caff844b
kienvo3105 <hvtrungkien@gmail.com>
2022-12-08 23:18:56 +0700

    seat component

---
+++ src/Pages/BookTicket/seat.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// export const seat = () => {
+//     return (
+//         <div style={{
+//             height: "30px",
+//             width: "37px",
+//             backgroundColor: "#FFFFFF",
+//             border: "1px solid black",
+//             borderRadius: "10% 10% 100% 100% / 10% 10% 100% 100%"
+//         }}>sdf</div >
+//     );
+// };
+
+class seat extends React.Component {
+    render() {
+        return (
+            <div style={{
+                height: "30px",
+                width: "37px",
+                backgroundColor: "#FFFFFF",
+                border: "1px solid black",
+                borderRadius: "10% 10% 100% 100% / 10% 10% 100% 100%"
+            }}>sdf</div >
+        );
+    }
+}
+export default seat;
