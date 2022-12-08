0882628e5742ee9393a6ef7f2326c643623c04f2
kienvo3105 <hvtrungkien@gmail.com>
2022-12-09 00:44:28 +0700

    update component seat

--- src/Pages/BookTicket/BookTicket.jsx
+++ src/Pages/BookTicket/BookTicket.jsx
@@ -2,13 +2,20 @@ import { Breadcrumb } from "antd";
 import React from "react";
 import { Layout } from "../../Layout/Layout";
 
+import { Seat } from "./Seat";
+
+
+
 export const BookTicket = () => {
   return (
     <Layout>
       <Breadcrumb style={{ marginLeft: "16px" }}>
         <Breadcrumb.Item>BookTicket</Breadcrumb.Item>
       </Breadcrumb>
-      <div className="p-[24px] min-h-[360px] bg-white m-[24px]">BookTicket</div>
+      <div className="p-[24px] min-h-[360px] bg-white m-[24px]">
+        <Seat state={0} className=" mb-[20px]"></Seat>
+        <Seat state={2}></Seat>
+      </div>
     </Layout>
   );
 };
--- src/Pages/BookTicket/seat.js
+++ src/Pages/BookTicket/seat.js
@@ -1,28 +1,22 @@
 import React from "react";
 
-// export const seat = () => {
-//     return (
-//         <div style={{
-//             height: "30px",
-//             width: "37px",
-//             backgroundColor: "#FFFFFF",
-//             border: "1px solid black",
-//             borderRadius: "10% 10% 100% 100% / 10% 10% 100% 100%"
-//         }}>sdf</div >
-//     );
-// };
-
-class seat extends React.Component {
-    render() {
-        return (
-            <div style={{
-                height: "30px",
-                width: "37px",
-                backgroundColor: "#FFFFFF",
-                border: "1px solid black",
-                borderRadius: "10% 10% 100% 100% / 10% 10% 100% 100%"
-            }}>sdf</div >
-        );
+export const Seat = (props) => {
+    const [state, setState] = React.useState(props.state);
+    const clicked = () => {
+        if (state === 0)
+            setState(1);
+        else if (state === 1)
+            setState(0);
     }
-}
-export default seat;
+    return (
+        <div style={{
+            margin: "5px",
+            height: "30px",
+            width: "37px",
+            backgroundColor: state === 0 ? "#B8C4BF" : state === 1 ? "#15D251" : "#313F36",
+            border: "1px solid black",
+            borderRadius: "10% 10% 100% 100% / 10% 10% 100% 100%"
+        }}
+            onClick={clicked}></div >
+    );
+};
