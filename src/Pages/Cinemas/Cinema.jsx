17ca0e08c2e7231d54942511e33fb18a023f6fbc
kienvo3105 <hvtrungkien@gmail.com>
2022-12-10 20:14:51 +0700

    show showtime for cinema page

--- src/Pages/Cinemas/Cinema.jsx
+++ src/Pages/Cinemas/Cinema.jsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import { Breadcrumb } from "antd";
-
+import { Breadcrumb, Space } from "antd";
+import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import { Layout } from "../../Layout/Layout";
-import { useParams } from "react-router-dom";
+
 import "./Cinema.css";
 import { useGet } from "../../api/get";
 
@@ -30,162 +30,18 @@ for (let i = 0; i <= 6; i++) {
   });
 }
 
-// const branch = [
-//   {
-//     name: "HẠNH PHÚC MÁU",
-//     id: 1,
-//     idCity: 1,
-//     idDate: 1,
-//     time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
-//   },
 
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 2,
-//     idCity: 1,
-//     idDate: 2,
-//     time: ["9h AM", "10h30 AM", "11h AM", "1h30 PM"],
-//   },
-
-//   {
-//     name: "HẠNH PHÚC MÁU",
-//     id: 3,
-//     idCity: 1,
-//     idDate: 3,
-//     time: ["11h15 AM", "4h30 PM"],
-//   },
-
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 4,
-//     idCity: 1,
-//     idDate: 1,
-//     time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
-//   },
-
-//   {
-//     name: "HẠNH PHÚC MÁU",
-//     id: 5,
-//     idCity: 1,
-//     idDate: 2,
-//     time: ["9h AM", "10h30 AM", "11h AM", "1h30 PM"],
-//   },
-
-//   {
-//     name: "HẠNH PHÚC MÁU",
-//     id: 6,
-//     idCity: 1,
-//     idDate: 3,
-//     time: ["11h15 AM", "4h30 PM"],
-//   },
-
-//   {
-//     name: "HẠNH PHÚC MÁU",
-//     id: 7,
-//     idCity: 2,
-//     idDate: 1,
-//     time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
-//   },
-
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 8,
-//     idCity: 2,
-//     idDate: 2,
-//     time: ["9h AM", "10h30 AM", "11h AM", "1h30 PM"],
-//   },
-
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 9,
-//     idCity: 2,
-//     idDate: 3,
-//     time: ["11h15 AM", "4h30 PM"],
-//   },
-
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 10,
-//     idCity: 2,
-//     idDate: 1,
-//     time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
-//   },
-
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 11,
-//     idCity: 2,
-//     idDate: 2,
-//     time: ["9h AM", "10h30 AM", "11h AM", "1h30 PM"],
-//   },
-
-//   {
-//     name: "TRO TÀN RỰC RỠ",
-//     id: 12,
-//     idCity: 2,
-//     idDate: 3,
-//     time: ["11h15 AM", "4h30 PM"],
-//   },
-// ];
 
 export const Cinema = () => {
-  const [provinceId, setProvinceId] = React.useState();
+  const navigate = useNavigate();
+  const [provinceId, setProvinceId] = React.useState("638f61dceae6921efd78e7b4");
   const [cinemaId, setCinemaId] = React.useState();
   const { fetchGet: fetchProvinces, result: provincesResult } = useGet();
   const { fetchGet: fetchProvince, result: provinceResult } = useGet();
   const { fetchGet: fetchShowtime, result: showtimeResult } = useGet();
   const [date, setDate] = React.useState(Dates[0].date);
+  const [movies, setMovies] = React.useState(0);
 
-  // Dữ liệu giả provines và cinemas
-  // const [provinces, setprovinces] = React.useState([
-  //   { name: "Toàn Quốc", id: 0 },
-  //   { name: "Hà Nội", id: 1 },
-  //   { name: "TP Hồ Chí Minh", id: 2 },
-  //   { name: "Cần Thơ", id: 3 },
-  //   { name: "Hải Phòng", id: 4 },
-  //   { name: "Đà Nẵng", id: 5 },
-  // ]);
-
-  // const [cinemas, setCinemas] = React.useState([
-  //   {
-  //     name: "BHD Hà Nội",
-  //     id: 6,
-  //     idProvince: 1,
-  //     address: "Lầu 5, Siêu Thị Vincom 3/2, 3C Đường 3/2, Quận 10, TPHCM",
-  //   },
-  //   {
-  //     name: "BHD Hà Nội 2",
-  //     id: 6,
-  //     idProvince: 1,
-  //     address: "Lầu 5, Siêu Thị Vincom 3/2, 3C Đường 3/2, Quận 10, TPHCM",
-  //   },
-  //   {
-  //     name: "BHD Hồ Chí Minh",
-  //     id: 7,
-  //     idProvince: 2,
-  //     address: "Lầu 5, Siêu Thị Vincom 3/2, 3C Đường 3/2, Quận 10, TPHCM",
-  //   },
-  //   {
-  //     name: "BHD Cần Thơ",
-  //     id: 8,
-  //     idProvince: 3,
-  //     address: "Lầu 5, Siêu Thị Vincom 3/2, 3C Đường 3/2, Quận 10, TPHCM",
-  //   },
-  //   {
-  //     name: "BHD Hải Phòng",
-  //     id: 9,
-  //     idProvince: 4,
-  //     address: "Lầu 5, Siêu Thị Vincom 3/2, 3C Đường 3/2, Quận 10, TPHCM",
-  //   },
-  //   {
-  //     name: "BHD Đà Nẵng",
-  //     id: 10,
-  //     idProvince: 5,
-  //     address: "Lầu 5, Siêu Thị Vincom 3/2, 3C Đường 3/2, Quận 10, TPHCM",
-  //   },
-  // ]);
-
-  
   // Load provinces từ Back-end
   // Load tất cả các tỉnh
   React.useEffect(() => {
@@ -198,51 +54,78 @@ export const Cinema = () => {
   }, [provinceId]);
 
   React.useEffect(() => {
-    if (provinceId) fetchShowtime(`showtime/cinema/${cinemaId}/${date}`);
-  }, [cinemaId, date]);
-  // Dữ liệu giả
-  // function getProvinceById(id) {
-  //   if (id != 0) {
-  //     const newList = cinema.filter(function (cinema) {
-  //       return cinema.idProvince == id;
+    fetchShowtime(`showtime/null/${provinceId}/${date}`);
+  }, [provinceId, date]);
+
+  // const getShowtimeofCinema = (id_cinema) => {
+  //   setCinemaId(id_cinema);
+  //   const showtime = showtimeResult.filter((item) => {
+  //     return item.cinema._id === id_cinema;
+  //   })
+  //   console.log(showtime);
+  //   let film = showtime[0].showtimes.map((item) => {
+  //     return item.movieId;
+  //   })
+  //   let filmId = film.map((film) => {
+  //     return film._id;
+  //   })
+  //   filmId = new Set(filmId);
+  //   filmId = [...filmId];
+  //   console.log(filmId);
+
+  //   const result = filmId.map((film) => {
+  //     const item = showtime[0].showtimes.filter((showtime) => {
+  //       return film === showtime.movieId._id;
   //     });
-  //     setCinemas([...newList]);
-  //   } else setCinemas([...cinema]);
+
+  //     const times = item.map((time) => { return time.time })
+  //     return {
+  //       movieId: item[0].movieId,
+  //       time: times,
+  //     }
+  //   })
+  //   console.log(result);
+  //   setMovies(result);
   // }
 
-  // Mặc định load tất cả cinema
+  React.useEffect(() => {
+    if (showtimeResult) {
+      console.log(showtimeResult)
+      const showtime = showtimeResult?.filter((item) => {
+        return item.cinema._id === cinemaId;
+      });
+      console.log(showtime);
+      let film = showtime[0]?.showtimes?.map((item) => {
+        return item.movieId;
+      })
+      let filmId = film?.map((film) => {
+        return film._id;
+      })
+      filmId = new Set(filmId);
+      filmId = [...filmId];
+      console.log(filmId);
 
-  const [mode, setMode] = useState("top");
-  const handleModeChange = (e) => {
-    setMode(e.target.value);
-  };
-
-  // Movie Showtime (cũ)
-
-  const [ShowTime, setShowTime] = React.useState([
-    {
-      name: "Hà Nội 1",
-      id: 1,
-      idCity: 1,
-      idDate: 1,
-      time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
-    },
-    {
-      name: "Hà Nội 2",
-      id: 4,
-      idCity: 1,
-      idDate: 1,
-      time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
-    },
-  ]);
-
-  const [buyTicket, setBuyTicket] = React.useState(false);
-
-  const param = useParams();
-  const [movie, setMovie] = useState(undefined);
-  const id = param.id;
-
-  console.log(showtimeResult);
+      const result = filmId.map((film) => {
+        const item = showtime[0]?.showtimes?.filter((showtime) => {
+          return film === showtime.movieId._id;
+        });
+        console.log("item", item);
+        const times = item.map((time) => {
+          return {
+            showtimeId: time._id,
+            time: time.time,
+          }
+        })
+        return {
+          movieId: item[0].movieId,
+          time: times,
+        }
+      })
+      console.log(result);
+      setMovies(result);
+    }
+  }, [showtimeResult, cinemaId]);
+  //console.log(showtimeResult);
   return (
     <Layout>
       <Breadcrumb style={{ marginLeft: "16px" }}>
@@ -269,22 +152,26 @@ export const Cinema = () => {
         {/* Hiển thị rạp tương ứng với tỉnh */}
         <div className="p-[24px] min-h-[360px] grid grid-cols-4 gap-10 px-[150px] text-black ">
           {provinceResult &&
-            provinceResult.cinemas.map((item) => (
-              <div
-                className="border-2 border-gray-500 h-[200px]"
-                onClick={() => setCinemaId(item._id)}
-              >
-                <div className="border-b-2 border-gray-500 mb-5 text-center py-[12px] font-bold">
-                  {item.name}
-                </div>
-                <div className="mb-5 px-[10px] h-[60px] text-xs">
-                  {item.address}
-                </div>
-                <div className="text-center py-[12px] border-t-2 border-gray-500">
-                  <FontAwesomeIcon size="lg" icon={faLocationDot} />
-                </div>
-              </div>
-            ))}
+            provinceResult.cinemas.map((item) => {
+
+              return (
+                <div
+                  className="border-2 border-gray-500 h-[200px]"
+                  onClick={() => setCinemaId(item._id)}
+                >
+
+                  <div className="border-b-2 border-gray-500 mb-5 text-center py-[12px] font-bold">
+                    {item.name}
+                  </div>
+                  <div className="mb-5 px-[10px] h-[60px] text-xs">
+                    {item.address}
+                  </div>
+                  <div className="text-center py-[12px] border-t-2 border-gray-500">
+                    <FontAwesomeIcon size="lg" icon={faLocationDot} />
+                  </div>
+                </div>)
+
+            })}
         </div>
 
         {/* Hiển thị Movies và Showtimes của Cinema */}
@@ -326,40 +213,49 @@ export const Cinema = () => {
             ></div>
           </div>
           <div className="p-[24px] min-h-[360px] bg-white my-[50px] mx-[200px]">
-            <div className="border-y-4 border-black py-5">
-              {Dates.map((item) => (
-                <button
-                  onClick={() => setDate(item.date)}
-                  type="button"
-                  key={item.id}
-                  className={`ml-5 border ${
-                    item.date === date ? "bg-red-500" : "bg-sky-700"
-                  }  hover:bg-sky-300 text-black h-[50px] w-[100px] rounded-xl`}
-                >
-                  {item.time}
-                </button>
-              ))}
+            <div className="border-t-4 border-black py-5">
+              {Dates.map((item) => {
+                return (
+                  <button
+                    onClick={() => {
+                      setDate(item.date);
+                    }}
+                    type="button"
+                    key={item.id}
+                    className={`ml-5 border ${item.date === date ? "bg-red-500" : "bg-sky-700"}  hover:bg-sky-300 text-black h-[50px] w-[100px] rounded-xl`}
+                  >
+                    {item.time}
+                  </button>
+                )
+              })}
+            </div>
+            <div>
+              {movies !== 0 && movies.map((movie) => {
+                return (
+                  <div className="border-t-2 border-slate-600 py-5 mx-[50px]">
+                    <div className="text-[30px] mb-[20px]">{movie.movieId.name}</div>
+                    <Space>
+                      {movie.time.map((temp, index) => (
+                        <div
+                          key={index}
+                          className="ml-5 border bg-gray-700 hover:bg-sky-300 text-white h-[50px] w-[100px] text-center pt-3 relative"
+                          onClick={() => {
+                            navigate(`/bookticket/${temp.showtimeId}`);
+                          }}
+                        >
+                          {temp.time}
+
+                        </div>
+                      ))}
+                    </Space>
+                  </div>
+                );
+              })}
             </div>
 
-            <div>
-              {ShowTime.map((item) => (
-                <div className="border-t-2 border-slate-600 py-5 mx-[50px]">
-                  <div className="text-[30px] mb-[20px]">{item.name}</div>
-                  {item.time.map((temp, index) => (
-                    <button
-                      type="button"
-                      key={index}
-                      className="ml-5 border bg-gray-700 hover:bg-sky-300 text-black h-[50px] w-[100px]"
-                    >
-                      {temp}
-                    </button>
-                  ))}
-                </div>
-              ))}
-            </div>
           </div>
         </>
       </div>
-    </Layout>
+    </Layout >
   );
 };
