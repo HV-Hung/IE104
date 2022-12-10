25a960682306655dfe57263c48b89b104aae2d9c
GaChip <gachip9090@gmail.com>
2022-12-10 21:07:19 +0700

    update code payment

--- src/Pages/Payment/PaymentVisa.jsx
+++ src/Pages/Payment/PaymentVisa.jsx
@@ -34,14 +34,40 @@ export const PaymentVisa = () => {
           <span>Quay lại</span>
         </div>
       </div>
-      <div className="flex-1 rounded-[6px]">
-        <div className="flex">
-          <span className="mr-[16px]">Thẻ tín dụng/ Ghi nợ</span>
+      <div className="flex-1 rounded-[6px] ml-[20px]">
+        <div className="flex bg-[#f0f0f0] px-[16px] py-[16px] mb-[20px]">
+          <span className="text-[20px] mr-[16px]">Thẻ tín dụng/ Ghi nợ</span>
           <img className="w-[32px] mr-[8px]" src="https://onepay.vn/paygate/assets/img/VS.svg" alt="" />
           <img className="w-[32px] mr-[8px]" src="https://onepay.vn/paygate/assets/img/MC.svg" alt="" />
           <img className="w-[32px] mr-[8px]" src="https://onepay.vn/paygate/assets/img/amex.svg" alt="" />
           <img className="w-[32px] mr-[8px]" src="https://onepay.vn/paygate/assets/img/JC.svg" alt="" />
         </div>
+          <div className="text-[16px] relative border-[1px] border-solid border-[#ccc] px-[16px] py-[16px] rounded-[2px]">
+            <div className="mb-[24px]">
+              <label htmlFor="number_card">
+                <p className="mb-[8px]" >Số thẻ*</p>
+              </label>
+              <input className="w-full px-[4px] py-[4px] border-[1px] border-solid border-[#ccc] rounded-[4px]" id="number_card" type="text" placeholder="1234 4568 9101 1234" />
+            </div>
+            <div className="flex justify-between mb-[48px]">
+              <div className="div">
+                <label htmlFor="date_card">
+                  <p className="mb-[8px]" >Tháng/Năm hết hạn*</p>
+                </label>
+                <input className="w-full px-[4px] py-[4px] border-[1px] border-solid border-[#ccc] rounded-[4px]" id="date_card" type="text" placeholder="12/25" />
+              </div>
+              <div className="div">
+                <label htmlFor="csc_card">
+                  <p className="mb-[8px]">CSC*</p>
+                </label>
+                <input className="w-full px-[4px] py-[4px] border-[1px] border-solid border-[#ccc] rounded-[4px]" id="csc_card" type="text" />
+              </div>
+            </div>
+            <div className="absolute bottom-[8px] right-[8px]">
+              <a className="text-[#16a8e1] mr-[16px]" href="">Hướng dẫn</a>
+              <button className="text-[#fff] px-[12px] py-[12px] bg-[#ccc]  rounded-[4px]" >Thanh toán</button>
+            </div>
+          </div>
       </div>
     </div>
       
