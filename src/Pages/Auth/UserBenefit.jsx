5f17dcb72eddc58bab8af9c8bb1c0d0827e76326
Hung <hoangvanhung6876@gmail.com>
2022-12-10 18:24:08 +0700

    add login

---
+++ src/Pages/Auth/UserBenefit.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+import "./Register.css";
+const UserBenefit = () => {
+  return (
+    <div id="processing" className="processing control ">
+      <nav>
+        <div role={"tablist"} className="nav-tabs" id="nav-tab">
+          <a role={"tab"} className="nav-item active" href="#">
+            <span>Mới</span>
+          </a>
+          <a role={"tab"} className="nav-item" href="#">
+            Đồng
+          </a>
+          <a role={"tab"} className="nav-item" href="#">
+            Bạc
+          </a>
+          <a role={"tab"} className="nav-item" href="#">
+            Vàng
+          </a>
+          <a role={"tab"} className="nav-item" href="#">
+            Kim Cương
+          </a>
+        </div>
+      </nav>
+
+      <div className="tab-content" id="nav-content">
+        <div id="nav-1" role={"tabpanel"} className="tab-pane">
+          <div className="cover-all-benefit row">
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/878_v5Membership2coffeeCup.png"
+                  alt=""
+                />
+                <span>Miễn phí upsize cho đơn đầu tiên</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="nav-2" role={"tabpanel"} className="tab-pane not-active">
+          <div className="cover-all-benefit">
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
+                  alt=""
+                />
+                <span>Tặng 01 phần bánh sinh nhật</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/879_v5Membership2snack.png"
+                  alt=""
+                />
+                <span>Miễn phí 01 phần Snack cho đơn hàng trên 100,000đ</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
+                  alt=""
+                />
+                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="nav-3" role={"tabpanel"} className="tab-pane not-active">
+          <div className="cover-all-benefit">
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
+                  alt=""
+                />
+                <span>Tặng 01 phần bánh sinh nhật</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/882_v5Membership2voucher.png"
+                  alt=""
+                />
+                <span>Ưu đãi Mua 2 tặng 1</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
+                  alt=""
+                />
+                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="nav-4" role={"tabpanel"} className="tab-pane not-active">
+          <div className="cover-all-benefit">
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
+                  alt=""
+                />
+                <span>Tặng 01 phần bánh sinh nhật</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/878_v5Membership2coffeeCup.png"
+                  alt=""
+                />
+                <span>Miễn phí 1 phần nước Cà phê / Trà</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
+                  alt=""
+                />
+                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="nav-5" role={"tabpanel"} className="tab-pane not-active">
+          <div className="cover-all-benefit">
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/876_v5Membership2bean.png"
+                  alt=""
+                />
+                <span>Được nhân 1.5 BEAN tích lũy</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
+                  alt=""
+                />
+                <span>Tặng 01 phần bánh sinh nhật</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/878_v5Membership2coffeeCup.png"
+                  alt=""
+                />
+                <span>Miễn phí 01 phần nước bất kì</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/882_v5Membership2voucher.png"
+                  alt=""
+                />
+                <span>
+                  Nhận riêng Ưu đãi từ The Coffee House và đối tác khác
+                </span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/880_v5Membership2star.png"
+                  alt=""
+                />
+                <span>Cơ hội trải nghiệm & hưởng đặc quyền đầu tiên</span>
+              </div>
+            </div>
+            <div className="all-benefit">
+              <div className="cover-image">
+                <img
+                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
+                  alt=""
+                />
+                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserBenefit;
