1518f24e74d325ae64b01125a7c39ec923894150
Do Hai <dohaizxc@gmail.com>
2022-12-11 10:21:39 +0700

    dsfad

--- src/Pages/Payment/PaymentMoMo.jsx
+++ src/Pages/Payment/PaymentMoMo.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,15 +8,6 @@ export const PaymentMoMo = () => {
       <div className="div"></div>
       <div className="div"></div>
     </div>
-        {/* <button
-          className="text-3xl absolute left-5 bottom-[20px]"
-          onClick={() => {
-            navigate(`/payment`);
-          }}
-        >
-          ⬅️ Quay lại
-        </button> */}
-      
   );
 };
 
