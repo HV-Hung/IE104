f3c4e17cb673b809d73bd32ef26d127c7418a73e
nguyenphilong <19521791@gm.uit.edu.vn>
2022-12-11 17:45:03 +0700

    Update login, register, profile

--- src/Pages/Auth/Profile.jsx
+++ src/Pages/Auth/Profile.jsx
@@ -4,13 +4,15 @@ import Barcode from "react-barcode";
 import "./Profile.css";
 import UserInfo from "./UserInfo";
 import UserBenefit from "./UserBenefit";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faClockRotateLeft, faGift } from "@fortawesome/free-solid-svg-icons";
 
 export const Profile = () => {
   const [tab, setTab] = React.useState(1);
   console.log(tab);
   return (
     <Layout>
-      <div className="container ">
+      <div className="profile-container ">
         <div className="column-left">
           <div className="user-info-card">
             <div className="point">
@@ -18,21 +20,18 @@ export const Profile = () => {
               <span>Tích điểm</span>
             </div>
             <div className="user-info-card-header">
-              <span>Nguyễn Phi</span>
-              <p>0 bean - Mới</p>
+              <span>Nguyễn Phi Long</span>
+              
             </div>
-            <div className="user-card-barcode">
-              <Barcode className="barcode" value="M161696901" />
-              <img
-                className="image"
-                src="https://order.thecoffeehouse.com/_nuxt/img/Leaves.5c9ad83.svg"
-                alt=""
-              />
+            <div className="user-card-barcode flex flex-col">
+              <Barcode className="barcode" displayValue={false}  format="CODE128" value="0393277584"  width='8px' height='180px'/>
+              <span className="font-bold mt-[5px] text-[20px]">0393277584</span>
             </div>
           </div>
 
           <div className="user-info-detail">
             <div className="user-rank">
+              <span>BẠC</span>
               <span>VÀNG</span>
               <span>KIM CƯƠNG</span>
             </div>
@@ -43,10 +42,11 @@ export const Profile = () => {
               <div className="dot-end"></div>
             </div>
 
-            <p className="user-text-bean">
-              Còn 100 BEAN nữa bạn sẽ thăng hạng. Đổi quà không ảnh hưởng tới
-              việc thăng hạng của bạn Chưa tích điểm
+            <p className="flex flex-row ml-[10px] mr-[10px] text-[24px] my-[20px] justify-between">
+             <span>Tổng chi tiêu</span>
+             <span className="font-bold">80.000.000 VNĐ</span>
             </p>
+            
           </div>
 
           <div className="user-menu">
@@ -55,45 +55,23 @@ export const Profile = () => {
                 onClick={() => setTab(1)}
                 className={`user-item ${tab === 1 && "active"}`}
               >
-                <img
-                  className="icon-user"
-                  src="https://order.thecoffeehouse.com/icon/user-icon.svg"
-                  alt=""
-                />
+                <FontAwesomeIcon className="text-[28px]" icon={faUser} />
                 <span>Thông tin tài khoản</span>
               </li>
+              
               <li
                 onClick={() => setTab(2)}
                 className={`user-item ${tab === 2 && "active"}`}
               >
-                <img
-                  className="icon-user"
-                  src="https://order.thecoffeehouse.com/icon/address-book.svg"
-                  alt=""
-                />
-                <span className="user-address">Sổ địa chỉ</span>
+                <FontAwesomeIcon className="text-[28px]" icon={faGift} />
+                <span>Quyền lợi thành viên</span>
               </li>
               <li
                 onClick={() => setTab(3)}
                 className={`user-item ${tab === 3 && "active"}`}
               >
-                <img
-                  className="icon-user"
-                  src="https://order.thecoffeehouse.com/icon/member-benefit.svg"
-                  alt=""
-                />
-                <span>Quyền lợi thành viên</span>
-              </li>
-              <li
-                onClick={() => setTab(4)}
-                className={`user-item ${tab === 4 && "active"}`}
-              >
-                <img
-                  className="icon-user"
-                  src="https://order.thecoffeehouse.com/icon/history-order.svg"
-                  alt=""
-                />
-                <span>Lịch sử mua hàng</span>
+                <FontAwesomeIcon className="text-[28px]" icon={faClockRotateLeft} />
+                <span>Lịch sử đặt vé</span>
               </li>
             </ul>
           </div>
@@ -101,14 +79,10 @@ export const Profile = () => {
 
         <div className="column-main">
           {tab === 1 && <UserInfo />}
-          {tab === 2 && (
-            <div id="address" className="address control">
-              <h1>Sổ địa chỉ</h1>
-            </div>
-          )}
+          
 
-          {tab === 3 && <UserBenefit />}
-          {tab === 4 && (<div id="card-product-note-item" className="card-product-note-item control">
+          {tab === 2 && <UserBenefit />}
+          {tab === 3 && (<div id="card-product-note-item" className="card-product-note-item control">
             <input type="text" name="" id="" />
           </div>)}
           </div>
--- src/Pages/Auth/UserBenefit.jsx
+++ src/Pages/Auth/UserBenefit.jsx
@@ -1,198 +1,89 @@
 import React from "react";
-import "./Register.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCakeCandles, faTicket, faGem } from "@fortawesome/free-solid-svg-icons";
+import "./Profile.css";
 const UserBenefit = () => {
   const [change, setChange] = React.useState(1);
   return (
     <div id="processing" className="processing control ">
       <nav>
-        <div role={"tablist"} className="nav-tabs" id="nav-tab">
+        <div role={"tablist"} className="nav-tabs font-bold text-[24px]" id="nav-tab">
+          
+          
           <a onClick={() => setChange(1)} className={`nav-item ${change === 1 && "active"}`} role={"tab"} href="#">
-            <span>Mới</span>
-          </a>
-          <a onClick={() => setChange(2)} className={`nav-item ${change === 2 && "active"}`} role={"tab"} href="#">
-            Đồng
-          </a>
-          <a onClick={() => setChange(3)} className={`nav-item ${change === 3 && "active"}`} role={"tab"} href="#">
             Bạc
           </a>
-          <a onClick={() => setChange(4)} className={`nav-item ${change === 4 && "active"}`} role={"tab"} href="#">
+          <a onClick={() => setChange(2)} className={`nav-item ${change === 2 && "active"}`} role={"tab"} href="#">
             Vàng
           </a>
-          <a onClick={() => setChange(5)} className={`nav-item ${change === 5 && "active"}`} role={"tab"} href="#">
+          <a onClick={() => setChange(3)} className={`nav-item ${change === 3 && "active"}`} role={"tab"} href="#">
             Kim Cương
           </a>
         </div>
       </nav>
 
       <div className="tab-content" id="nav-content">
-        {change === 1 && (<div id="nav-1" role={"tabpanel"} className="tab-pane">
-          <div className="cover-all-benefit row">
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/878_v5Membership2coffeeCup.png"
-                  alt=""
-                />
-                <span>Miễn phí upsize cho đơn đầu tiên</span>
-              </div>
-            </div>
-          </div>
-        </div>)}
+        
 
-        {change === 2 && (<div id="nav-2" role={"tabpanel"} className="tab-pane">
+        
+
+        {change === 1 && (<div id="nav-3" role={"tabpanel"} className="tab-pane ">
           <div className="cover-all-benefit">
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
-                  alt=""
-                />
-                <span>Tặng 01 phần bánh sinh nhật</span>
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faCakeCandles} />
+                <span>Tặng 01 phần bánh sinh nhật tại rạp</span>
               </div>
             </div>
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/879_v5Membership2snack.png"
-                  alt=""
-                />
-                <span>Miễn phí 01 phần Snack cho đơn hàng trên 100,000đ</span>
-              </div>
-            </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
-                  alt=""
-                />
-                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
-              </div>
-            </div>
-          </div>
-        </div>)}
-
-        {change === 3 && (<div id="nav-3" role={"tabpanel"} className="tab-pane ">
-          <div className="cover-all-benefit">
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
-                  alt=""
-                />
-                <span>Tặng 01 phần bánh sinh nhật</span>
-              </div>
-            </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/882_v5Membership2voucher.png"
-                  alt=""
-                />
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faTicket} />
                 <span>Ưu đãi Mua 2 tặng 1</span>
               </div>
             </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
-                  alt=""
-                />
-                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
-              </div>
-            </div>
+            
           </div>
         </div>)}
 
-        {change === 4 && (<div id="nav-4" role={"tabpanel"} className="tab-pane ">
-          <div className="cover-all-benefit">
+        {change === 2 && (<div id="nav-4" role={"tabpanel"} className="tab-pane ">
+        <div className="cover-all-benefit">
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
-                  alt=""
-                />
-                <span>Tặng 01 phần bánh sinh nhật</span>
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faCakeCandles} />
+                <span>Tặng 01 phần bánh sinh nhật tại rạp</span>
               </div>
             </div>
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/878_v5Membership2coffeeCup.png"
-                  alt=""
-                />
-                <span>Miễn phí 1 phần nước Cà phê / Trà</span>
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faTicket} />
+                <span>Nhận 5 vé xem phim miễn phí</span>
               </div>
             </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
-                  alt=""
-                />
-                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
-              </div>
-            </div>
+            
           </div>
         </div>)}
 
-        {change === 5 && (<div id="nav-5" role={"tabpanel"} className="tab-pane ">
-          <div className="cover-all-benefit">
+        {change === 3 && (<div id="nav-5" role={"tabpanel"} className="tab-pane ">
+        <div className="cover-all-benefit">
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/876_v5Membership2bean.png"
-                  alt=""
-                />
-                <span>Được nhân 1.5 BEAN tích lũy</span>
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faCakeCandles} />
+                <span>Tặng 01 phần bánh sinh nhật tại rạp</span>
               </div>
             </div>
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/877_v5Membership2birthdayCake.png"
-                  alt=""
-                />
-                <span>Tặng 01 phần bánh sinh nhật</span>
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faTicket} />
+                <span>Nhận 10 vé xem phim miễn phí</span>
               </div>
             </div>
             <div className="all-benefit">
               <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/878_v5Membership2coffeeCup.png"
-                  alt=""
-                />
-                <span>Miễn phí 01 phần nước bất kì</span>
+              <FontAwesomeIcon className="text-[50px] mb-[20px] blue-color" icon={faGem} />
+                <span>Phòng chờ riêng cho thành viên kim cương</span>
               </div>
             </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/882_v5Membership2voucher.png"
-                  alt=""
-                />
-                <span>
-                  Nhận riêng Ưu đãi từ The Coffee House và đối tác khác
-                </span>
-              </div>
-            </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/880_v5Membership2star.png"
-                  alt=""
-                />
-                <span>Cơ hội trải nghiệm & hưởng đặc quyền đầu tiên</span>
-              </div>
-            </div>
-            <div className="all-benefit">
-              <div className="cover-image">
-                <img
-                  src="https://minio.thecoffeehouse.com/image/tchmobileapp/881_v5Membership2store.png"
-                  alt=""
-                />
-                <span>Đặc quyền Đổi Ưu đãi bằng điểm BEAN tích lũy</span>
-              </div>
-            </div>
+            
           </div>
         </div>)}
       </div>
